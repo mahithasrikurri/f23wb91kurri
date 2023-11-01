d50f8f3f43405852ac0a6b0b8e22a9ba85349f22
Kurri <S566991@nwmissouri.edu>
2023-11-01 15:44:51 -0500

    added code in board.pug

--- routes/board.js
+++ routes/board.js
@@ -3,11 +3,12 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  let query = req.query
-  console.log(`rows ${query.rows}`)
-  console.log(`cols ${query.cols}`)
+  let query = req.query;
+  console.log(`rows ${query.rows}`);
+  console.log(`cols ${query.cols}`);
 
-  res.render('board', { title: 'Board Display', query: query});
+  res.render('board', { title: 'Board Display',query:query });
+
 });
 
 module.exports = router;
